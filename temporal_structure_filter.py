164fb3017652310d2e31cce8b4366815ce7e4cd6
cxqj <43702702+cxqj@users.noreply.github.com>
2019-10-06 17:20:35 +0800

    Update temporal_structure_filter.py

--- temporal_structure_filter.py
+++ temporal_structure_filter.py
@@ -36,49 +36,69 @@ class TSF(nn.Module):
         """
 
         # scale to length of videos
-        centers = (length - 1) * (center + 1) / 2.0   # 相当于把中心点和delta按时间维度展开
-        deltas = length * (1.0 - torch.abs(delta))
+	# 下面都是6是因为batch_size为2，每一个batch为3
+        centers = (length - 1) * (center + 1) / 2.0   #[6]
+        deltas = length * (1.0 - torch.abs(delta)) # [6]
 
-        gammas = torch.exp(1.5 - 2.0 * torch.abs(gamma))
+        gammas = torch.exp(1.5 - 2.0 * torch.abs(gamma)) # [6]
         
-        a = Variable(torch.zeros(self.Ni))
+        a = Variable(torch.zeros(self.Ni)) 
         a = a.cuda()
         
         # stride and center
-        a = deltas[:, None] * a[None, :]
+        a = deltas[:, None] * a[None, :]  # None可以理解为按某一行或一列依次运行 [6,3]
         a = centers[:, None] + a
 
-        b = Variable(torch.arange(0, time))
+        b = Variable(torch.arange(0, time))  # [0,1,2,3,......31]  [32,]
         b = b.cuda()
-        
-        f = b - a[:, :, None]
+        '''
+	b = [0,1,2,.............31]
+	
+	a = [20.1715,20.1715,20.1715]
+	    [6.1892,6.1892,6.1892]
+	    [19.0191,19.0191,19.0191]
+	    
+	    
+	    
+	b-a[:,:,None] = [[0,1,2,...31]-[20.1715]
+	                [0,1,2,...31]-[20.1715]
+			[0,1,2,...31]-[20.1715]]
+			[[0,1,2,...31]-[6.1892]
+			 [0,1,2,...31]-[6.1892]
+			 [0,1,2,...31]-[6.1892]]
+			[[0,1,2,...31]-[19.0191]
+			 [0,1,2,...31]-[19.0191]
+			 [0,1,2,...31]-[19.0191]]
+	
+	'''
+        f = b - a[:, :, None]   # [6,3,32]
         f = f / gammas[:, None, None]
         
         f = f ** 2.0
         f += 1.0
         f = np.pi * gammas[:, None, None] * f
         f = 1.0/f
-        f = f/(torch.sum(f, dim=2) + 1e-6)[:,:,None]
+        f = f/(torch.sum(f, dim=2) + 1e-6)[:,:,None]   # (6,3,32),对第二个维度聚合，相当于对所有的数值进行了归一化
 
-        f = f[:,0,:].contiguous()
+        f = f[:,0,:].contiguous()     # 由于第一个维度是一样的，因此移除了第一个维度 (6,32)
 
-        f = f.view(-1, self.Ni, time)
+        f = f.view(-1, self.Ni, time)  # (2,3,32)
         
         return f
 
     def forward(self, inp):
-        video, length = inp
-        batch, channels, time = video.squeeze(3).squeeze(3).size()  # （B,C,T,H,W）
+        video, length = inp  # (B,C,T,H,W)，length为传入的mask向量长度和时间维度一致
+        batch, channels, time = video.squeeze(3).squeeze(3).size()  # 
         # vid is (B x C x T)
-        vid = video.view(batch*channels, time, 1).unsqueeze(2)
-        # f is (B x T x N)
+        vid = video.view(batch*channels, time, 1).unsqueeze(2) # (B*C,T,1,1)--
+        # f is (B x N x T)
         f = self.get_filters(torch.tanh(self.delta).repeat(batch), torch.tanh(self.gamma).repeat(batch), torch.tanh(self.center.repeat(batch)), length.view(batch,1).repeat(1,self.Ni).view(-1), time)
         # repeat over channels
-        f = f.unsqueeze(1).repeat(1, channels, 1, 1)
-        f = f.view(batch*channels, self.Ni, time)
+        f = f.unsqueeze(1).repeat(1, channels, 1, 1)  # (2,1024,3,32)-->(B,C,N,T)
+        f = f.view(batch*channels, self.Ni, time)  # (B*C,N,T)
 
         # o is (B x C x N)
-        o = torch.bmm(f, vid.squeeze(2))  # 这里的滤波器并不是卷积，而是矩阵相乘
+        o = torch.bmm(f, vid.squeeze(2))  # (B*C,N,T)X(B*C,T,1) = (B*C,N,1)
         del f
         del vid
         o = o.view(batch, channels*self.Ni)#.unsqueeze(3).unsqueeze(3)
