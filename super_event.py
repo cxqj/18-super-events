b6a6b459b16d6b4a2ef96d912cd9bcb2da8847af
cxqj <43702702+cxqj@users.noreply.github.com>
2020-06-14 10:38:11 +0800

    Update super_event.py

--- super_event.py
+++ super_event.py
@@ -15,7 +15,7 @@ class SuperEvent(nn.Module):
         self.add_module('d', self.dropout)
 
         # 定义两个超事件
-        self.super_event = tsf.TSF(3)  # 3可以理解为通道数，其实超事件可以理解为深度可分离卷积，对每个通道用一个卷积核做卷积，每个卷积核的参数分布满足柯西分布
+        self.super_event = tsf.TSF(3)
         self.add_module('sup', self.super_event)
         self.super_event2 = tsf.TSF(3)
         self.add_module('sup2', self.super_event2)
@@ -24,21 +24,22 @@ class SuperEvent(nn.Module):
         # we have 2xD*3
         # we want to learn a per-class weighting
         # to take 2xD*3 to D*3
-        self.cls_wts = nn.Parameter(torch.Tensor(classes))  # soft_attention权重
+        self.cls_wts = nn.Parameter(torch.Tensor(classes))  # (65)
         
         # 超事件的权重
-        self.sup_mat = nn.Parameter(torch.Tensor(1, classes, 1024*3))  # class为滤波器的个数，其中每个滤波器有三个通道，作用于输入的每个通道
+        self.sup_mat = nn.Parameter(torch.Tensor(1, classes, 1024*3))  #(1,65,1024*3)
         stdv = 1./np.sqrt(1024+1024)
         self.sup_mat.data.uniform_(-stdv, stdv)
 
-        # 最终的输出和其卷积核参数的分布
-        self.per_frame = nn.Conv3d(1024, classes, (1,1,1))
+        # 获取输入特征对应的帧级分类结果，最后和输出的超事件表达相结合
+        self.per_frame = nn.Conv3d(1024, classes, (1,1,1))  # 1024-->65
         self.per_frame.weight.data.uniform_(-stdv, stdv)
         self.per_frame.bias.data.uniform_(-stdv, stdv)
         self.add_module('pf', self.per_frame)
         
-    def forward(self, inp):
+    def forward(self, inp):  # inp[0]:(2,1024,32,1,1)  inp[1]:[32,32] inp[1]记录了特征的实际长度
         inp[0] = self.dropout(inp[0])
+        
         # 判断当前是否为验证集
         val = False
         dim = 1
@@ -46,23 +47,21 @@ class SuperEvent(nn.Module):
             val = True
             dim = 0
 
-        #print inp[0].size()
-        # 最终的超事件表示是两种超事件的concat
-        # 每个超事件的维度为(B,C*3),两个超事件对应论文中的M
-        super_event = self.dropout(torch.stack([self.super_event(inp).squeeze(), self.super_event2(inp).squeeze()], dim=dim))  # (B,2,C*3)
+        # 每个超事件的输出为(2,3072),两个超事件的输出拼接后为(2,2,3072)
+        super_event = self.dropout(torch.stack([self.super_event(inp).squeeze(), self.super_event2(inp).squeeze()], dim=dim))  # (2,2,3072)
         if val:
             super_event = super_event.unsqueeze(0)
+            
         # we have B x 2 x D*3
         # we want B x C x D*3
-
-        #print super_event.size()
+        #接下来需要获取每个类别对应的超事件，就是通过cls_wts矩阵与超事件的输出相乘即可得到
         # now we have C x 2 matrix
         cls_wts = torch.stack([torch.sigmoid(self.cls_wts), 1-torch.sigmoid(self.cls_wts)], dim=1)
 
         # now we do a bmm to get B x C x D*3
         #print cls_wts.expand(inp[0].size()[0], -1, -1).size(), super_event.size()
         # (2,65,2) X (2,2,3072)---->(2,65,3072)
-        super_event = torch.bmm(cls_wts.expand(inp[0].size()[0], -1, -1), super_event)    # 再用类别权重对每个通道加权
+        super_event = torch.bmm(cls_wts.expand(inp[0].size()[0], -1, -1), super_event)    # 获取每个类别对应的超事件
         del cls_wts
         print super_event.size()
         # apply the super-event weights
